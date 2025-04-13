5a943009169eda357646b8361e7212c8aad238a5
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-12 23:38:25 -0600

    FEAT : Add SiteType entity for categorizing places

---
+++ NationalParks/Models/SiteType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NationalParks.Models;
+
+public partial class SiteType
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+         
+    public virtual ICollection<Place> Places { get; set; } = new List<Place>();
+}
