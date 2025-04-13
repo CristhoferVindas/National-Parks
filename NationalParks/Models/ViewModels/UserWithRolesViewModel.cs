1f4f2658291623c8f3573bf6cd99bd2d64001504
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-12 23:46:12 -0600

    FEAT: Add UserWithRolesViewModel for displaying user information with their roles

---
+++ NationalParks/Models/ViewModels/UserWithRolesViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace NationalParks.Models.ViewModels
+{
+    public class UserWithRolesViewModel
+    {
+        public User User { get; set; }
+        public List<string> Roles { get; set; }
+    }
+}
