82a0011b80eda9ee2715552553b8d95832f96abd
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-12 23:39:17 -0600

    FEAT : Introduce TicketSale entity for tracking sales transactions

---
+++ NationalParks/Models/TicketSale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NationalParks.Models;
+
+public partial class TicketSale
+{
+    public int Id { get; set; }
+
+    public decimal TotalPrice { get; set; }
+
+    public DateTime SaleDate { get; set; }
+
+    public string Customer { get; set; } = null!;
+
+    public int? CardId { get; set; }
+
+    public int? VisitorId { get; set; }
+
+    public virtual Card? Card { get; set; }
+
+    public virtual ICollection<Ticket> Tickets { get; set; } = new List<Ticket>();
+
+    public virtual Visitor? Visitor { get; set; }
+}
