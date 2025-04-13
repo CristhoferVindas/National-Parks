35a03043127b2ac451e432b07615b8bf7bd3f5f9
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-12 23:36:25 -0600

    FEAT : Add Card entity for payment method management

---
+++ NationalParks/Models/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NationalParks.Models;
+
+public partial class Card
+{
+    public int Id { get; set; }
+
+    public string CardType { get; set; } = null!;
+
+    public int Commission { get; set; }
+
+    public virtual ICollection<TicketSale> TicketSales { get; set; } = new List<TicketSale>();
+}
