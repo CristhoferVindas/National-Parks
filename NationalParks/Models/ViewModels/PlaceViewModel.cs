b44db6342f73ba26c8cb96a9bbe4c338cfa5da92
Cristhofer Eli Vindas Muñoz <120971218+CristhoferVindas@users.noreply.github.com>
2025-04-12 23:45:39 -0600

    FEAT : Introduce PlaceViewModel for managing place creation and editing

---
+++ NationalParks/Models/ViewModels/PlaceViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace NationalParks.Models.ViewModels
+{
+    public class PlaceViewModel
+    {
+        public Place Place { get; set; }
+        public List<SiteType> SiteTypes { get; set; }
+        public List<Location> Locations { get; set; }
+    }
+}
